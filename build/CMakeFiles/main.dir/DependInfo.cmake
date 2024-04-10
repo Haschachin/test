a2de6c8c3d3d4d63b669d9aa664bc6aa368466d5
Chin <xxxshark@foxmail.com>
2024-04-10 11:58:33 +0800

    TEST

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/Haschachin/Documents/VSCODE/main/src/main.c" "CMakeFiles/main.dir/src/main.c.obj" "gcc" "CMakeFiles/main.dir/src/main.c.obj.d"
+  "E:/Haschachin/Documents/VSCODE/main/src/print.c" "CMakeFiles/main.dir/src/print.c.obj" "gcc" "CMakeFiles/main.dir/src/print.c.obj.d"
+  "E:/Haschachin/Documents/VSCODE/main/src/print1.c" "CMakeFiles/main.dir/src/print1.c.obj" "gcc" "CMakeFiles/main.dir/src/print1.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/main.c.obj"
+  "CMakeFiles/main.dir/src/main.c.obj.d"
+  "CMakeFiles/main.dir/src/print.c.obj"
+  "CMakeFiles/main.dir/src/print.c.obj.d"
+  "CMakeFiles/main.dir/src/print1.c.obj"
+  "CMakeFiles/main.dir/src/print1.c.obj.d"
+  "E:/Haschachin/Documents/VSCODE/main/bin/libmain.dll.a"
+  "E:/Haschachin/Documents/VSCODE/main/bin/main.exe"
+  "E:/Haschachin/Documents/VSCODE/main/bin/main.exe.manifest"
+  "E:/Haschachin/Documents/VSCODE/main/bin/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
